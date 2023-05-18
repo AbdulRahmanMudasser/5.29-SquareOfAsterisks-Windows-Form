ae0056eff9e6b5bb1c2acda98c4bcad4c81c86af
Abdul Rahman <abdulrahmanmuddasser@gmail.com>
2023-05-18 16:01:47 +0500

    solution done

--- Square of Asterisks Windows Form/Form1.Designer.cs
+++ Square of Asterisks Windows Form/Form1.Designer.cs
@@ -28,13 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.sizeTextBox = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(123, 105);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(83, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter Size";
+            // 
+            // sizeTextBox
+            // 
+            this.sizeTextBox.Location = new System.Drawing.Point(309, 99);
+            this.sizeTextBox.Name = "sizeTextBox";
+            this.sizeTextBox.Size = new System.Drawing.Size(100, 26);
+            this.sizeTextBox.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(193, 209);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(149, 83);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Display Square";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.sizeTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox sizeTextBox;
+        private System.Windows.Forms.Button button1;
     }
 }
 
--- Square of Asterisks Windows Form/Form1.cs
+++ Square of Asterisks Windows Form/Form1.cs
@@ -16,5 +16,41 @@ namespace Square_of_Asterisks_Windows_Form
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int size = Convert.ToInt32(sizeTextBox.Text);
+            string square = "";
+
+            if (size < 1)
+            {
+                size = 1;
+            }
+
+            if (size > 20)
+            {
+                size = 20;
+            }
+
+            for (int row = 1; row <= size; row++) 
+            {
+                for (int col = 1; col <= size; col++)
+                {
+                    if (row == 1 || row == size || col == 1 || col == size) 
+                    {
+                        square += "*";
+                    }
+
+                    else
+                    {
+                        square += " ";
+                    }
+                }
+
+                square += "\n";
+            }
+
+            MessageBox.Show(square);
+        }
     }
 }
